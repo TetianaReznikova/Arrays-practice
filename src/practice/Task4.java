c9346c6499d1a6b8c30c97041e00bfd707f6f820
TetianaReznikova <reznikovat2705@gmail.com>
2023-09-08 23:30:49 +0300

    Adding Task4

--- src/practice/Task4.java
+++ src/practice/Task4.java
@@ -1,4 +1,20 @@
 package practice;
 import java.util.Scanner;
 public class Task4 {
+    static String[] fruit;
+
+    public static void main(String[] args) {
+        fruit = new String[]{"Oranges", "Apples", "Bananas", "Pears", "Pineapples", "Limes"};
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your question\n");
+        System.out.print("Have you bought some: ");
+        String boughtFruit = sc.nextLine();
+        for (String fr : fruit) {
+            if (fr.equals(boughtFruit))
+                System.out.printf("Yes, I have. I've bought some %s",
+                        fr);
+
+        }
+        sc.close();
+    }
 }
